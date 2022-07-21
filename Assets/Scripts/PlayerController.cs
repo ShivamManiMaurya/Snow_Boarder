53dde7d51b1091f04f71363e6dc7aeb00ba67141
Shivam Maurya <109658264+ShivamManiMaurya@users.noreply.github.com>
2022-07-21 15:39:39 +0530

    Change in Barry script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,15 +5,43 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float torqueAmount = 1f;
+    [SerializeField] float surfaceSpeed = 30f;
+    [SerializeField] float decreaseSpeed = 10f;
+    [SerializeField] float baseSpeed = 20f;
+
     Rigidbody2D rb2d;
+    SurfaceEffector2D surfaceEffector2D;
 
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        surfaceEffector2D = FindObjectOfType<SurfaceEffector2D>();
     }
 
     // Update is called once per frame
     void Update()
+    {
+        RotatePlayer();
+        BoostSpeed();
+    }
+
+    void BoostSpeed()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            surfaceEffector2D.speed = surfaceSpeed;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            surfaceEffector2D.speed = decreaseSpeed;
+        }
+        else
+        {
+            surfaceEffector2D.speed = baseSpeed;
+        }
+    }
+    
+    void RotatePlayer()
     {
         if (Input.GetKey(KeyCode.A))
         {
